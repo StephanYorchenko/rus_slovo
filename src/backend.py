81b8e8adf695037f3b405a085d6995cbade56440
Stepan Yurchenko <stepan33314@yandex.ru>
2019-04-12 22:19:27 +0500

    VERY BIG CHANGES in working with messages

--- src/backend.py
+++ src/backend.py
@@ -5,12 +5,12 @@ import json
 import sqlite3
 
 
-
 class Question:
 
-    def __init__(self, word, answer):
+    def __init__(self, word, answer, peer):
         self.word = word
         self.answer = answer
+        self.peer = peer
 
     @staticmethod
     def reformat_word(word, k=None):
@@ -24,8 +24,7 @@ class Question:
         random.shuffle(res_list)
         return res_list
 
-    @staticmethod
-    def get_json_keyboard(list_answers, exit_but=False):
+    def get_json_keyboard(self, list_answers, exit_but=False):
         result_dict = {'one_time': False,
                        'buttons': []}
         for i in list_answers:
@@ -40,26 +39,41 @@ class Question:
             result_dict['buttons'].append(k)
         if exit_but:
             exit = [
-                    {
-                    'action':{
+                {
+                    'action': {
                         'type': 'text',
                         'label': 'Стоп'
-                        },
-                    'color':'negative'
-                    }
-                    ]
+                    },
+                    'color': 'negative'
+                }
+            ]
             result_dict['buttons'].append(exit)
-        with open('keyboards/keyboard_test.json', 'w', encoding='UTF-8') as f:
+        with open(f'keyboards/{self.peer}.json', 'w', encoding='UTF-8') as f:
             f.write(json.dumps(result_dict, indent=4, ensure_ascii=False))
 
+    def check(self, answer):
+        return answer == self.answer
+
+
+class Task:
+    def __init__(self, peer, task=0):
+        self.task = self.question_creator(self.selector(task), peer) if task else 0
+        self.current_task = 0
+        self.right = 0
+
     @staticmethod
-    def selector(table_name):
+    def selector(index):
         con = sqlite3.connect(r'src/rus_slovo.db')
-        sql = f'SELECT * FROM {table_name}'
+        sql = "SELECT word, answer FROM orfo_dictation"
+        if index:
+            sql += f'WHERE index_task = {index}'
         cur = con.cursor()
-        c = list(cur.execute(sql))
+        c = random.shuffle(list(cur.execute(sql)))
         cur.fetchall()
         cur.close()
         con.close()
-        return {x[1]: x[2] for x in c}
+        return c[:32]
 
+    @staticmethod
+    def question_creator(array, peer):
+        return [Question(i[0], i[1], peer) for i in array]
