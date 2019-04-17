13659b25ac78b8295036881a8edd74a363642415
Stepan Yurchenko <stepan33314@yandex.ru>
2019-04-17 23:43:19 +0500

    Go back

--- src/server.py
+++ src/server.py
@@ -5,93 +5,105 @@ from vk_api.bot_longpoll import VkBotEventType
 
 from src import backend
 
+from collections import defaultdict
+
 
 class Server:
-    keyboards = [['keyboards/keyboard_home.json', 'Вы находитесь в главном меню'],
-                 ['keyboards/keyboard_type_dictation.json', 'Какой диктант предпочтёте писать?'],
-                 ['keyboards/keyboard_none.json', 'Рад Вас видеть!'],
-                 ['keyboards/keyboard_mode_dictation.json', 'Потренируемся или напишем контрольную?'],
-                 ['keyboards/keyboard_task.json', 'Выберите задание из предложенного списка:'],
-                 ['keyboards/keyboard_start.json', 'Нажмите, чтобы начать'],
-                 ['keyboards/keyboard_next.json', 'Выберите дальнейшее действие']
-                 ]
-
+    keyboards = ['keyboards/keyboard_home.json',
+                 'keyboards/keyboard_type_dictation.json',
+                 'keyboards/keyboard_none.json',
+                 'keyboards/keyboard_mode_dictation.json',
+                 'keyboards/keyboard_task.json']
+    messages = {'start': 'Рад Вас видеть!',
+                'home': 'Вы находитесь в главном меню',
+                'about': 'Разработано Drackeland Technology',
+                'type_dictation': 'Какой диктант предпочтёте писать?',
+                'mode_dictation': 'Потренируемся или напишем контрольную?',
+                'choose_task': 'Выберите задание из предложенного списка:'
+                }
+    #users = defaultdict(lambda: [-1, backend.Task()])
     users = backend.UserDict()
-
 
     def __init__(self, token, group_id):
         self.username = ''
         self.cur_keyboard = 0
+        self.cur_mes = 'start'
         self.vk = vk_api.VkApi(token=token)
         self.long_poll = VkBotLongPoll(self.vk, group_id)
         self.vk_api = self.vk.get_api()
         self.active = 0
         self.random_id = 0
 
-    def send_msg(self, send_id, message=None, keyboard_index=0):
-        if message:
-            if self.users[send_id][0] == 6:
+    def send_msg(self, send_id, message, keyboard_index=0):
+        if self.users[send_id][0] in {5, 6}:
+            try:
                 self.vk_api.messages.send(peer_id=send_id,
                                           message=message,
                                           random_id=self.random_id,
                                           keyboard=open(f'keyboards/{send_id}.json', 'r',
                                                         encoding='UTF-8').read())
-            elif self.users[send_id][0] == 5:
+            except FileNotFoundError:
                 self.vk_api.messages.send(peer_id=send_id,
-                                          message=self.keyboards[5][1],
+                                          message=message,
                                           random_id=self.random_id,
-                                          keyboard=open(self.keyboards[5][0], "r", encoding="UTF-8").read())
+                                          keyboard=open(self.keyboards[2], "r", encoding="UTF-8").read())
         else:
             self.vk_api.messages.send(peer_id=send_id,
-                                      message=self.keyboards[keyboard_index][1],
+                                      message=message,
                                       random_id=self.random_id,
-                                      keyboard=open(self.keyboards[keyboard_index][0], "r", encoding="UTF-8").read())
+                                      keyboard=open(self.keyboards[keyboard_index], "r", encoding="UTF-8").read())
         self.random_id += 1
 
     def start(self):
         print('@home')
         for event in self.long_poll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
-                print(self.users, event.type, event.object.text, sep='     ')
+                print(self.users, event.type, event.object.text,  sep='     ')
                 peer = event.object.peer_id
                 self.users[peer][2] += 1
                 if self.users[peer][0] not in {5, 6}:
                     if self.users[peer][0] == -1:
-                        self.send_msg(peer, keyboard_index=2)
-                        self.keyboards[2][1] = 'Разрабоотано DvaTopora'
+                        self.send_msg(peer,
+                                      self.messages['start'],
+                                      2)
                         self.users[peer][0] = 0
+                        self.cur_mes = 'home'
                     if event.object.text == 'Диктант' and not self.users[peer][0]:
+                        self.cur_mes = 'type_dictation'
                         self.users[peer][0] = 1
                     elif event.object.text == 'О боте' and not self.users[peer][0]:
-                        self.send_msg(peer, keyboard_index=2)
+                        self.send_msg(peer, self.messages['about'], 2)
                     elif event.object.text == 'Орфоэпический' and self.users[peer][0] == 1:
+                        self.cur_mes = 'mode_dictation'
                         self.users[peer][0] = 3
                     elif event.object.text == 'Контрольная':
+                        self.cur_mes = 'choose_task'
                         self.users[peer][0] = 4
                     elif event.object.text == 'Назад':
                         if self.users[peer][0] == 1:
                             self.users[peer][0] = 0
+                            self.cur_mes = 'home'
                         elif self.users[peer][0] == 3:
                             self.users[peer][0] = 1
+                            self.cur_mes = 'type_dictation'
                         elif self.users[peer][0] == 4:
                             self.users[peer][0] = 3
+                            self.cur_mes = 'mode_dictation'
                     elif self.users[peer][0] == 4:
                         a = ["Августовский ... занял",
                              "Заняла ... нарвала"]
                         res = a.index(event.object.text)
                         self.start_cont(res + 1, peer)
                         self.users[peer][0] = 5
-                        self.send_msg(peer, '1')
+                        self.send_msg(peer,
+                                      'Отправьте любой символ чтобы начать',
+                                      self.users[peer][0])
                         continue
-                    elif self.users[peer][0] == 7:
-                        if event.object.text == "Попробовать заново":
-                            self.users[peer][0] = 4
-                        else:
-                            self.users[peer][0] = 0
-                    self.send_msg(peer, keyboard_index=self.users[peer][0])
+                    self.send_msg(peer, self.messages[self.cur_mes], self.users[peer][0])
                 elif self.users[peer][0] == 5:
                     self.users[peer][1].task[0].get_json_keyboard(exit_but=True)
                     self.send_msg(peer, self.users[peer][1].task[0].word)
+                    print('----')
                     self.users[peer][0] = 6
                 else:
                     if event.object.text != 'Стоп':
@@ -108,14 +120,14 @@ class Server:
                             self.users[peer][1].task[kk].get_json_keyboard(exit_but=True)
                             self.send_msg(peer, self.users[peer][1].task[kk].word)
                         else:
-                            self.users[peer][0] = 7
+                            self.users[peer][0] = 0
                             self.send_msg(peer,
                                           f'{self.get_user_name(peer)}, Ваш результат {self.users[peer][1].right}/32',
-                                          keyboard_index=5)
+                                          keyboard_index=0)
                     else:
-                        self.users[peer][0] = 7
+                        self.users[peer][0] = 0
                         self.send_msg(peer, f'{self.get_user_name(peer)}, Ваш результат {self.users[peer][1].right}/32',
-                                      keyboard_index=5)
+                                      keyboard_index=0)
 
     def get_user_name(self, user_id):
         """ Получаем имя пользователя"""
