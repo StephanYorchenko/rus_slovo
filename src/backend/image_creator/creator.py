1be5cc1fd660c3eb1765bd9f975ffd1a126ee1d4
Stepan Yurchenko <stepan33314@yandex.ru>
2019-04-27 19:23:42 +0500

    Class to converting html

--- src/backend/image_creator/creator.py
+++ src/backend/image_creator/creator.py
@@ -1,6 +1,19 @@
 import imgkit
 from jinja2 import Environment, FileSystemLoader
 
+
+class HTMLConventor:
+    def __init__(self, html_name):
+        self.loader = FileSystemLoader('./templates')
+        self.env = Environment(loader=self.loader, trim_blocks=True, lstrip_blocks=True)
+        self.template = self.env.get_template(html_name)
+
+    @staticmethod
+    def render(name, right, all, text):
+        k = template.render(name=name, right=right, all=all, text=text)
+        imgkit.from_string(k, 'out.jpg', css='./styles/style.css')
+
+
 if __name__ == '__main__':
     loader = FileSystemLoader('./templates')
     env = Environment(loader=loader, trim_blocks=True, lstrip_blocks=True)
@@ -8,5 +21,3 @@ if __name__ == '__main__':
 
     k = template.render(name='Григорий', right=10, all=16, text='Родительный падеж')
     imgkit.from_string(k, 'out.jpg', css='./styles/style.css')
-
-
